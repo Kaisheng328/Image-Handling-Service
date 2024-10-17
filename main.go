aca892eb975495db168234708deadd62ff9b262f
Kaisheng328 <kaisheng1801@gmail.com>
2024-10-17 09:49:12 +0800

    Second commit

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"Project/configs"
 	"Project/routes"
+	"fmt"
 	_ "image/png" // Import image packages to support PNG, JPEG, etc.
 )
 
@@ -11,17 +12,5 @@ func main() {
 	configs.InitiEnvConfigs()
 	routes.InitializeRoutes()
 	routes.Router.Run(":" + configs.EnvConfigs.LocalServerPort)
-	// r := gin.Default()
-
-	// // Pass clients to the route handlers
-	// r.POST("/v1/health", func(c *gin.Context) {
-	// 	routes.HandlePost(c)
-	// })
-
-	// r.POST("/v1/health/Hi", func(c *gin.Context) {
-	// 	routes.HandleWatermarkImage(c, StorageClient, FirestoreClient)
-	// })
-
-	// // Start the server
-	// r.Run(":5000")
+	fmt.Printf("hi")
 }
