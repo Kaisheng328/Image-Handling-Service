d988bda6cdef3167209ca64037cc4f35c25e980b
Kaisheng328 <rubykaikai@hotmail.com>
2024-10-18 09:32:31 +0800

    6 initial

--- routes/route.go
+++ routes/route.go
@@ -17,6 +17,7 @@ var (
 	Router          = gin.Default()
 	StorageClient   *storage.Client
 	FirestoreClient *firestore.Client
+	latestStatus    string = "API is working fine !!!!"
 )
 
 func InitializeRoutes() {
@@ -41,6 +42,7 @@ func HandlePost(c *gin.Context) {
 	sa := option.WithCredentialsFile("halogen-device-438608-v9-firebase-adminsdk-kwtb8-780d822bbb.json")
 	StorageClient, err := storage.NewClient(ctx, sa)
 	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Credential File"})
 		fmt.Printf("error initializing app: %v\n", err)
 		return
 	}
@@ -48,6 +50,7 @@ func HandlePost(c *gin.Context) {
 	// Initialize Firebase Storage
 	FirestoreClient, err := firestore.NewClient(ctx, "halogen-device-438608-v9", sa, option.WithEndpoint("asia-southeast1-firestore.googleapis.com:443"))
 	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error initializing Firestore Client"})
 		fmt.Printf("Error initializing Firestore client: %v\n", err)
 		return
 	}
@@ -56,6 +59,7 @@ func HandlePost(c *gin.Context) {
 	// Parse the request body
 	if err := c.BindJSON(&requestBody); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		fmt.Println("Invalid Request Body")
 		return
 	}
 
@@ -67,8 +71,9 @@ func HandlePost(c *gin.Context) {
 	}
 	currentTime := time.Now()
 	timestamp := currentTime.Format("20060102_150405")
+	latestStatus = fmt.Sprintf("image_%v uploaded successfully", timestamp)
 	c.JSON(http.StatusOK, gin.H{
-		"status": fmt.Sprintf("image_%v uploaded successfully", timestamp),
+		"status": latestStatus,
 	})
 }
 func HandleWatermarkImage(c *gin.Context) {
@@ -87,6 +92,7 @@ func HandleWatermarkImage(c *gin.Context) {
 	// Initialize Firebase Storage
 	FirestoreClient, err := firestore.NewClient(ctx, "halogen-device-438608-v9", sa, option.WithEndpoint("asia-southeast1-firestore.googleapis.com:443"))
 	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error initializing Firestore Client"})
 		fmt.Printf("Error initializing Firestore client: %v\n", err)
 		return
 	}
@@ -101,11 +107,11 @@ func HandleWatermarkImage(c *gin.Context) {
 	err = functions.ProcessImageWithWatermark(requestBody.ImageID, StorageClient, FirestoreClient)
 
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to Process"})
 		return
 	}
-
+	latestStatus = fmt.Sprintf("watermarked_%s saved successfully", requestBody.ImageID)
 	c.JSON(http.StatusOK, gin.H{
-		"status": fmt.Sprintf("watermarked_%s saved successfully", requestBody.ImageID),
+		"status": latestStatus,
 	})
 }
